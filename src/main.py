d4cbc330943d29c64c322b092cbb97df6273e25d
Shomendeb <pre.qua888@gmail.com>
2026-06-13 22:48:42 +0600

    Add multiplication function to utils and call it in main

--- src/main.py
+++ src/main.py
@@ -1,7 +1,15 @@
 import datetime
+from utils import add, subtract
 
-# Print your name
-print("Name: Shomen")
-
-# Print today's date
+# Previous code from Task 3
+print("Name: Shomen Deb")
 print("Today's Date:", datetime.date.today())
+
+# Calling the new calculator functions
+result_add = add(5, 3)
+result_sub = subtract(10, 4)
+result_mult = multiply(4, 7)  # Added this line
+
+print(f"5 + 3 = {result_add}")
+print(f"10 - 4 = {result_sub}")
+print(f"4 * 7 = {result_mult}")  # Added this line
